52ebe88141a140693b26e4771af31ef65a7d3c1e
Jacob Pacheco <72306809+JacobPacheco100@users.noreply.github.com>
2022-06-29 15:24:45 -0400

    current:features red underline wip

---
+++ app.js
@@ -0,0 +1,65 @@
+// features
+
+const ft_info = [
+  {
+    img: "assets/images/illustration-features-tab-1.svg",
+    h1: "Bookmark in one click",
+    p: "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favorite sites",
+  },
+  {
+    img: "assets/images/illustration-features-tab-2.svg",
+    h1: "Intelligent search",
+    p: "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
+  },
+  {
+    img: "assets/images/illustration-features-tab-3.svg",
+    h1: "Share your bookmarks",
+    p: "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
+  },
+]
+
+const features = document.getElementById("features") //create for each
+const ft_img = document.querySelector(".ft-img")
+const ft_h1 = document.querySelector(".ft-h1")
+const ft_p = document.querySelector(".ft-p")
+
+features.addEventListener("click", (event) => {
+  const e = event.target
+
+  if (e.classList.contains("SB")) {
+    ft_img.src = ft_info[0].img
+    ft_h1.textContent = ft_info[0].h1
+    ft_p.textContent = ft_info[0].p
+  } else if (e.classList.contains("SS")) {
+    ft_img.src = ft_info[1].img
+    ft_h1.textContent = ft_info[1].h1
+    ft_p.textContent = ft_info[1].p
+  } else {
+    ft_img.src = ft_info[2].img
+    ft_h1.textContent = ft_info[2].h1
+    ft_p.textContent = ft_info[2].p
+  }
+})
+
+// red underline
+
+const sb = document.querySelector(".SB")
+const ss = document.querySelector(".SS")
+const es = document.querySelector(".ES")
+
+features.addEventListener("click", (event) => {
+  const e = event.target
+  if (e.classList.contains("SB")) {
+    sb.classList.add("bb-red")
+    ss.classList.remove("bb-red")
+    es.classList.remove("bb-red")
+  } else if (e.classList.contains("SB")) {
+    sb.classList.remove("bb-red")
+    ss.classList.add("bb-red")
+    es.classList.remove("bb-red")
+  } else {
+    sb.classList.remove("bb-red")
+    ss.classList.remove("bb-red")
+    es.classList.add("bb-red")
+  }
+})
